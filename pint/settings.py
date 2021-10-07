86a79fdbcf9ba0f8140f852b3ef91761e1bb39d1
GyungA <gga612@naver.com>
2021-10-08 05:45:02 +0900

    django course 22 commit

--- pint/settings.py
+++ pint/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import environ, os
+from django.urls import reverse_lazy
 
 env = environ.Env(
     # set casting, default value
@@ -135,6 +136,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
